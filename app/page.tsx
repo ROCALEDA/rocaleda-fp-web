665017bce6596bfea69f4bccaf59909092fb2d80
Roberto <w.parrav@uniandes.edu.co>
2023-10-09 19:25:36 -0300

    Home

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,32 @@
-import { Button } from "@mui/material";
+import Image from 'next/image';
 
 export default function Home() {
-  return <div>
-  <Button variant="contained" color="primary">Primary</Button>
-  <Button variant="contained" color="secondary">Secundary</Button>
-  <Button variant="contained" color="info">Info</Button>
-  <Button variant="contained" color="success">Success</Button>
-  <Button variant="contained" color="warning">Warning</Button>
-  <Button variant="contained" color="error">Error</Button>
-  </div>
+  return (
+      <>
+      <div style={{
+          zIndex: -1,
+          position: 'fixed',
+          width: '100%',
+          height: '100vh'
+      }}>
+      <Image
+      src="/images/background.png"
+      layout="fill"
+      objectFit="cover"
+      alt="Cloud Background Image"
+      />
+      </div>
+      <div style={{
+        display: 'flex',
+        alignItems: 'center',
+        fontSize: '4rem',
+        justifyContent: 'center',
+        height: '100vh',
+        zIndex: 1,
+        position: 'relative'
+    }}>
+        <h1>Pronto...</h1>
+    </div>
+      </>
+  )
 }
