9f58c2997ce6e87b41835ea948081bafa59640d3
Roberto-Parra <w.parrav@uniandes.edu.co>
2023-10-15 20:30:16 -0300

    layout candidates

---
+++ app/candidate/page.tsx
@@ -0,0 +1,8 @@
+import AuthLayout from "@/components/layout/auth-layout";
+
+export default function LoginPage() {
+    return (
+      <AuthLayout text="Candidatos" textColor='#009EF8'>
+      </AuthLayout>
+    );
+  }
