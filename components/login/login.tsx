ff15695846cd0d4f9d53b264500c2e9a0cf3a2b9
Daniela Castellanos <ld.castellanos@uniandes.edu.co>
2023-10-15 12:54:49 -0500

    Api config and toasts

--- components/login/login.tsx
+++ components/login/login.tsx
@@ -1,22 +1,36 @@
-import Image from "next/image";
+"use client";
+import Link from "next/link";
 import {
   Box,
   Card,
   Grid,
-  Typography,
-  TextField,
   Stack,
   Button,
+  TextField,
+  Typography,
 } from "@mui/material";
+import { getSomeData } from "@/api/apiService";
+import { enqueueSnackbar } from "notistack";
 
 export default function Login() {
+  const onLogin = async () => {
+    try {
+      const { data, status } = await getSomeData();
+      console.log(data);
+      enqueueSnackbar(`Sesión iniciada (${status})`, { variant: "success" });
+    } catch (err) {
+      console.log("error", err);
+      enqueueSnackbar("Error", { variant: "error" });
+    }
+  };
+
   return (
     <Grid container spacing={2} padding={2}>
       <Grid item xs={12} md={6} gap={2}>
         <Stack direction="column" alignItems="center">
-          <Box width="100%" maxWidth={2 / 3}>
+          <Box sx={{ maxWidth: "500px", width: "100%" }}>
             <Card>
-              <Box padding={4} textAlign="center">
+              <Box padding={4} textAlign="center" width="100%">
                 <Typography variant="h5">Iniciar sesión</Typography>
                 <Box padding={4}>
                   <Stack direction="column" spacing={6}>
@@ -32,7 +46,9 @@ export default function Login() {
                       autoComplete="current-password"
                       variant="standard"
                     />
-                    <Button variant="contained">Ingresar</Button>
+                    <Button variant="contained" onClick={() => onLogin()}>
+                      Ingresar
+                    </Button>
                   </Stack>
                 </Box>
               </Box>
@@ -51,12 +67,16 @@ export default function Login() {
               Aún no tienes una cuenta?
             </Typography>
             <Stack direction="row" spacing={2}>
-              <Button sx={{ backgroundColor: "#F4E8C9", color: "black" }}>
-                Quiero ser candidato
-              </Button>
-              <Button sx={{ backgroundColor: "#F4E8C9", color: "black" }}>
-                Soy una empresa
-              </Button>
+              <Link href="/registro-candidato" passHref>
+                <Button sx={{ backgroundColor: "#F4E8C9", color: "black" }}>
+                  Quiero ser candidato
+                </Button>
+              </Link>
+              <Link href="/registro-cliente" passHref>
+                <Button sx={{ backgroundColor: "#F4E8C9", color: "black" }}>
+                  Soy una empresa
+                </Button>
+              </Link>
             </Stack>
           </Box>
         </Stack>
