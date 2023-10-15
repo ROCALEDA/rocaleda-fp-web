a02cc27a60e1e11f67b851dbf94ac1726c796466
Roberto-Parra <w.parrav@uniandes.edu.co>
2023-10-15 15:12:36 -0300

    update ci/cd

--- app/ThemeRegistry.tsx
+++ app/ThemeRegistry.tsx
@@ -5,7 +5,7 @@ import { CacheProvider } from '@emotion/react';
 import { ThemeProvider } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
 import { useState } from 'react';
-import { theme } from '@/app/theme/theme';
+import { theme } from './theme/theme';
 
 
 export default function ThemeRegistry(props: any) {
