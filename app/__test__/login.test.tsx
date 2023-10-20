30ae3ab9735dc4dd0e65ddc3707a4599d5ad64ae
Daniela Castellanos <ld.castellanos@uniandes.edu.co>
2023-10-19 23:19:41 -0500

    Login tests, template

---
+++ app/__test__/login.test.tsx
@@ -0,0 +1,42 @@
+// Login.test.js
+import React from "react";
+import { render, fireEvent, waitFor } from "@testing-library/react";
+import { login } from "@/api/apiService";
+import Login from "@/components/login/login";
+
+describe("<Login />", () => {
+  test("renders the login form", () => {
+    const { getByText } = render(<Login />);
+    // expect(getByText("Iniciar sesión")).toBeInTheDocument();
+  });
+
+  test("shows error on invalid submission", async () => {
+    const { getByText, getByLabelText, findByText } = render(<Login />);
+    const emailInput = getByLabelText("Correo");
+    const passwordInput = getByLabelText("Password");
+    const submitButton = getByText("Ingresar");
+
+    // Simulate a form submission without entering data
+    fireEvent.click(submitButton);
+
+    // Assert error messages show up
+    // await waitFor(() => {
+    //   expect(findByText("Email is required")).toBeInTheDocument();
+    //   expect(findByText("Password is required")).toBeInTheDocument();
+    // });
+  });
+
+  test("calls the login function on valid submission", async () => {
+    const mockResponse = { data: { token: "fake_token" }, status: 200 };
+
+    const { getByText, getByLabelText } = render(<Login />);
+    const emailInput = getByLabelText("Correo");
+    const passwordInput = getByLabelText("Password");
+    const submitButton = getByText("Ingresar");
+
+    fireEvent.change(emailInput, { target: { value: "test@example.com" } });
+    fireEvent.change(passwordInput, { target: { value: "password" } });
+
+    fireEvent.click(submitButton);
+  });
+});
