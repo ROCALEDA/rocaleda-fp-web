8e4daa20269a82bd05e886a466ff75f85dcaed06
Camilo Sanchez <casc3798@gmail.com>
2023-10-22 14:32:50 -0500

    Add fallback api url

--- api/config.ts
+++ api/config.ts
@@ -1,3 +1,3 @@
-const API_URL = process.env.NEXT_PUBLIC_API_URL;
+const API_URL = process.env.NEXT_PUBLIC_API_URL || 'https://rocaleda-fp-api-gateway-dot-proyecto-final-abc-jobs-g9.uc.r.appspot.com';
 
 export default API_URL;
