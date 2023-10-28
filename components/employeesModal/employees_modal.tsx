5b4632e3c57bdfc823db347c5599b47ac599558b
Roberto-Parra <w.parrav@uniandes.edu.co>
2023-10-28 20:00:44 -0300

    validation employees modal

--- components/employeesModal/employees_modal.tsx
+++ components/employeesModal/employees_modal.tsx
@@ -1,6 +1,7 @@
 "use client"
 import React, { useState } from 'react';
 import { Modal, Box, Typography, TextField, Button } from '@mui/material';
+import * as yup from 'yup';
 
 interface EmployeesModalProps {
     open: boolean;
@@ -9,18 +10,41 @@ interface EmployeesModalProps {
 }
 
 
+const employeesValidationSchema = yup.object().shape({
+    name: yup.string()
+    .required("El nombre del funcionario es requerido")
+    .max(100, "El nombre del funcionario no puede tener más de 100 caracteres"),
+    role: yup.string()
+    .required("El nombre del perfil es requerido")
+    .max(30, "El nombre del perfil asociado no puede tener más de 30 caracteres")
+});
+
+
+
 const EmployeesModal: React.FC<EmployeesModalProps> = ({ open, onClose, onAddEmployee }) => {
     const [employeeName, setEmployeeName] = useState("");
     const [employeeRole, setEmployeeRole] = useState("");
+    const [errors, setErrors] = React.useState<{ [key: string]: string }>({});
+
     const addEmployee = () => {
-        if (employeeName && employeeRole) {
-            const newEmployee = {name: employeeName,role: employeeRole};
+    const newEmployee = { name: employeeName, role: employeeRole };
+    
+    employeesValidationSchema.validate(newEmployee, { abortEarly: false })
+        .then(() => {
             onAddEmployee(newEmployee);
             setEmployeeName("");
             setEmployeeRole("");
+            setErrors({});
             onClose();
-        }
-    };
+        })
+        .catch((error: yup.ValidationError) => {
+            const validationErrors: { [key: string]: string } = {};
+            error.inner.forEach(err => {
+                validationErrors[err.path!] = err.message;
+            });
+            setErrors(validationErrors);
+        });
+};
 
     return (
         <Modal open={open} onClose={onClose}>
@@ -44,6 +68,8 @@ const EmployeesModal: React.FC<EmployeesModalProps> = ({ open, onClose, onAddEmp
             variant="standard"
             value={employeeName}
             onChange={e => setEmployeeName(e.target.value)} 
+            error={Boolean(errors.name)}
+            helperText={errors.name} 
         />
         <TextField 
             label="Nombre del perfil" 
@@ -51,20 +77,22 @@ const EmployeesModal: React.FC<EmployeesModalProps> = ({ open, onClose, onAddEmp
             margin="normal" 
             variant="standard"
             value={employeeRole}
-            onChange={e => setEmployeeRole(e.target.value)} 
+            onChange={e => setEmployeeRole(e.target.value)}
+            error={Boolean(errors.role)}
+            helperText={errors.role}  
         />
         <Box sx={{ display: 'flex', justifyContent: 'space-between', mt: 5 }}>
             <Button variant="outlined" onClick={onClose}>
-              CANCELAR
+            CANCELAR
             </Button>
             <Button 
             variant="contained"
             onClick={addEmployee} 
             sx={{ px: 3,backgroundColor: "#A15CAC", "&:hover": { backgroundColor: "#864D8F" } }}
             >
-              AÑADIR
+            AÑADIR
             </Button>
-          </Box>
+        </Box>
 
         </Box>
         </Modal>
