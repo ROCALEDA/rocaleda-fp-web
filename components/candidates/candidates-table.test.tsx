8d599121b5e5d8eeb3a026a1aa93554da5783f28
Daniela Castellanos <ld.castellanos@uniandes.edu.co>
2023-11-05 13:22:17 -0500

    Candidates pagination

---
+++ components/candidates/candidates-table.test.tsx
@@ -0,0 +1,24 @@
+import { render } from "@testing-library/react";
+import "@testing-library/jest-dom";
+import CandidatesTable from "./candidates-table";
+
+global.fetch = jest.fn(() =>
+  Promise.resolve({
+    json: () => Promise.resolve({ data: [], total_pages: 0 }),
+  })
+);
+// Mock the necessary hooks and modules
+jest.mock("next-auth/react", () => ({
+  useSession: jest.fn(() => ({ data: { user: { token: "mock-token" } } })),
+}));
+jest.mock("next/navigation", () => ({
+  useRouter: jest.fn(() => ({ pathname: "/somepath" })),
+  useSearchParams: jest.fn(() => new URLSearchParams()),
+}));
+
+describe("CandidatesTable", () => {
+  it("renders without crashing", () => {
+    const { getByText } = render(<CandidatesTable />);
+    expect(getByText("Cargando...")).toBeInTheDocument();
+  });
+});
