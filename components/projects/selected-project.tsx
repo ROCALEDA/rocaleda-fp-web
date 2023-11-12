59744f69819ead1a8a70a89d9597db92dd8d43de
Roberto <w.parrav@uniandes.edu.co>
2023-11-12 17:08:33 -0300

    interfaz seleccionar candidatos en un proyecto

--- components/projects/selected-project.tsx
+++ components/projects/selected-project.tsx
@@ -1,7 +1,8 @@
 "use client";
 import React, {useState} from 'react';
-import { Box, Card, CardContent, Typography, Chip, Accordion, AccordionSummary, AccordionDetails } from '@mui/material';
-import PeopleIcon from '@mui/icons-material/PersonOutlineOutlined';
+import { Box, Card, CardContent, Typography, Chip, Accordion, AccordionSummary, AccordionDetails, FormControlLabel,Checkbox,Button } from '@mui/material';
+//import PeopleIcon from '@mui/icons-material/PersonOutlineOutlined';
+import WorkspacePremiumIcon from '@mui/icons-material/WorkspacePremium';
 import { useTheme } from '@mui/material/styles';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import useMediaQuery from '@mui/material/useMediaQuery';
@@ -10,6 +11,7 @@ interface Position {
     id: number;
     is_open: boolean;
     name: string;
+    candidates: Candidate[];
 }
   
   interface Project {
@@ -23,11 +25,53 @@ interface Position {
     project: Project | null;
   }
 
+  interface Candidate {
+    id: number;
+    name: string;
+  }
+  
+
+
 export default function SelectedProject({ project }: SelectedProjectProps) {
     const theme = useTheme();
     const isSmallScreen = useMediaQuery(theme.breakpoints.down('xs'));
 
+    const [selectedCandidates, setSelectedCandidates] = useState<Record<number, number[]>>({});
+    const [selectionCompleted, setSelectionCompleted] = useState<Record<number, boolean>>({});
+
+    
+
     if (!project) return null;
+
+    const mockCandidates = [
+        { id: 1, name: 'Stephany Jimenez' },
+        { id: 2, name: 'Carlos Rodriguez' },
+        { id: 3, name: 'Isabel Ramirez' },
+    ];
+
+    const positionsWithCandidates = project.positions.map(pos => ({
+        ...pos,
+        candidates: mockCandidates 
+    }));
+
+    const handleSelectCandidate = (positionId: number, candidateId: number, isSelected: boolean) => {
+        setSelectedCandidates(prevSelected => {
+            const selectedForPosition = prevSelected[positionId] || [];
+            if (isSelected) {
+                return { ...prevSelected, [positionId]: [...selectedForPosition, candidateId] };
+            } else {
+                return { ...prevSelected, [positionId]: selectedForPosition.filter(id => id !== candidateId) };
+            }
+        });
+    };
+    
+
+    const handleSelectionComplete = (positionId: number) => {
+        setSelectionCompleted(prevState => ({
+            ...prevState,
+            [positionId]: !prevState[positionId],
+        }));
+    };
 
     return (
         <Box mt={8} display="flex" justifyContent="center">
@@ -44,13 +88,9 @@ export default function SelectedProject({ project }: SelectedProjectProps) {
             </Box>
                 {/* Acordeón con las posiciones disponibles */}
             <Box display="flex" flexDirection="column"  mt={2} mb={10}>
-                {project.positions.filter(pos => pos.is_open).map((position) => (
+                {positionsWithCandidates.filter(pos => pos.is_open).map((position) => (
                     <Accordion key={position.id} style={{ marginBottom: '5px' }}>
-                        <AccordionSummary
-                            expandIcon={<ExpandMoreIcon />}
-                            aria-controls={`panel${position.id}-content`}
-                            id={`panel${position.id}-header`}
-                        >
+                        <AccordionSummary expandIcon={<ExpandMoreIcon />} aria-controls={`panel${position.id}-content`} id={`panel${position.id}-header`}>
                             <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'space-between', width: '100%'}}>
                             <div style={{ display: 'flex', flexDirection: 'row', alignItems: 'flex-start' }}>
                             <Typography color="textSecondary">Perfil - {position.name}</Typography>
@@ -59,6 +99,50 @@ export default function SelectedProject({ project }: SelectedProjectProps) {
                             </div>
                         </AccordionSummary>
                         <AccordionDetails>
+                            {/* checkbox y boton seleccionar*/}
+                            <Box sx={{ display: selectionCompleted[position.id] ? 'block' : 'none' }}>
+                                {selectedCandidates[position.id]?.map(candidateId => {
+                                    const candidate = mockCandidates.find(c => c.id === candidateId);
+                                    return (
+                                        <Typography key={candidateId} style={{color: '#718096', fontSize: 16}}>
+                                            <WorkspacePremiumIcon style={{ verticalAlign: 'middle', color: '#F3DA90' }} />
+                                            {' '}{candidate?.name}
+                                        </Typography>
+                                    );
+                                })}
+                            </Box>
+                            <Box sx={{ display: selectionCompleted[position.id] ? 'none' : 'flex', flexDirection: 'column' }}>
+                            {position.candidates.map(candidate => (
+                                <FormControlLabel
+                                    key={candidate.id}
+                                    style={{color: '#718096', fontSize: 16}}
+                                    control={
+                                        <Checkbox
+                                            checked={selectedCandidates[position.id]?.includes(candidate.id) || false}
+                                            onChange={(event) => handleSelectCandidate(position.id, candidate.id, event.target.checked)}
+                                            sx={{
+                                                
+                                                '&.Mui-checked': {
+                                                    color: '#A15CAC',
+                                                },
+                                            }}
+                                        />
+                                    }
+                                    label={candidate.name}
+                                />
+                            ))}
+                                <Box sx={{ width: 'fit-content', mt: 2 }}> 
+                                    <Button
+                                    type="submit"
+                                    variant="contained" 
+                                    sx = {{backgroundColor: "#A15CAC","&:hover": { backgroundColor: "#864D8F" }}} 
+                                    //onClick={() => console.log('Seleccionados:', selectedCandidates[position.id])}
+                                    onClick={() => handleSelectionComplete(position.id)}
+                                    >
+                                    Seleccionar
+                                    </Button>
+                                </Box>
+                            </Box>
                         </AccordionDetails>
                     </Accordion>
                 ))}
