a9aab6ff4f15b8d13c3c65fda649a62f1b95f283
Roberto <w.parrav@uniandes.edu.co>
2023-11-25 15:22:52 -0300

    Fix user_id type

--- utils/test-utils.tsx
+++ utils/test-utils.tsx
@@ -14,7 +14,7 @@ function customRender(
     const mockSession = {
       user: {
         name: "Test User",
-        user_id: "1",
+        user_id: 1,
         email: "test@example.com",
         role_id: userRole,
         token: "Fake token",
