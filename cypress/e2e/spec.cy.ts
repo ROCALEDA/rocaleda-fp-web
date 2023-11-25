00222214cda290a74d8a934406be6aee47fa2b9c
Roberto <w.parrav@uniandes.edu.co>
2023-11-25 17:44:17 -0300

    login and register test

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -1,15 +1,29 @@
-describe('Login', () => {
-  it('Login Visit', () => {
-    cy.visit('https://fp-web-service-mecna653na-uc.a.run.app/es/login')
-  })
-})
+import { faker } from "@faker-js/faker";
 
-describe('Login Test', () => {
-  it('logs in successfully', () => {
-    cy.visit('https://fp-web-service-mecna653na-uc.a.run.app/es/login'); // Cambia la URL a la de tu app
-    cy.get('input[name="email"]').type('c.toros.customer@example.com');
-    cy.get('input[name="password"]').type('cristian');
-    cy.get('button[type="submit"]').click();
-    cy.url().should('include', '/home'); // Asegúrate de que la URL cambie al dashboard después del inicio de sesión
+describe('Login Page', () => {
+  beforeEach(() => {
+    cy.visit('/es/login');
   });
-});
+
+  const userEmail = faker.internet.email();
+  const userPassword = 'Test1234!';
+
+  it('Client/Enterprise Register', () => {
+    cy.get('[data-cy=candidate-signup]').click();
+
+    cy.get('input[name="company"]').type(faker.company.name());
+    cy.get('input[name="email"]').type(userEmail);
+    cy.get('input[name="phone"]').type(faker.phone.number('###############'));
+    cy.get('input[name="password"]').type(userPassword);
+    cy.get('input[name="password2"]').type(userPassword);
+    cy.get('[data-cy=candidate-register]').click();
+    cy.url().should('include', '/es/login'); 
+  });
+  it('login client/enterprise in successfully', () => {
+    cy.get('input[name="email"]').type(userEmail);
+    cy.get('input[name="password"]').type(userPassword);
+    cy.get('button[type="submit"]').click();
+    cy.url().should('include', '/home'); 
+  });
+
+})
