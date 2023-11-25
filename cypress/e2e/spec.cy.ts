1c86848171da7d47790c2ccef09fbeb2994a0306
Roberto <w.parrav@uniandes.edu.co>
2023-11-25 16:19:24 -0300

    e2e Login test

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -1,5 +1,15 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
+describe('Login', () => {
+  it('Login Visit', () => {
+    cy.visit('https://fp-web-service-mecna653na-uc.a.run.app/es/login')
   })
 })
+
+describe('Login Test', () => {
+  it('logs in successfully', () => {
+    cy.visit('https://fp-web-service-mecna653na-uc.a.run.app/es/login'); // Cambia la URL a la de tu app
+    cy.get('input[name="email"]').type('c.toros.customer@example.com');
+    cy.get('input[name="password"]').type('cristian');
+    cy.get('button[type="submit"]').click();
+    cy.url().should('include', '/home'); // Asegúrate de que la URL cambie al dashboard después del inicio de sesión
+  });
+});
